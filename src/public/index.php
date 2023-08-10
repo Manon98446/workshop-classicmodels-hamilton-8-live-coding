ceeb2ba5194630ffc7e231a25d7b2f9ae83edd5f
Broodco <broodcoorens.maxime@gmail.com>
2023-08-10 12:23:30 +0200

    ADD ProductController.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,24 +1,10 @@
 <?php
 declare(strict_types=1);
 
+require 'public/controllers/ProductController.php';
+
 session_start();
 
-try {
-    // 1 - Connexion à la DB
-    require_once 'public/db/Database.php';
-    $db = new Database();
+$productController = new ProductController();
 
-    // 2 - Requête SQL pour récupérer la liste des produits
-
-    $stmt = $db->query(
-        "SELECT * FROM products LIMIT 20"
-    );
-    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    // 3 - Affichage de la liste des produits
-    include 'public/views/layout/header.view.php';
-    include 'public/views/index.view.php';
-    include 'public/views/layout/footer.view.php';
-} catch (Exception $e) {
-    print_r($e->getMessage());
-}
+$productController->index();
--- src/public/product.php
+++ src/public/product.php
@@ -1,33 +1,10 @@
 <?php
 declare(strict_types=1);
 
+require 'public/controllers/ProductController.php';
+
 session_start();
 
-try {
-    // 1 - Connexion à la DB
-    require_once 'public/db/Database.php';
+$productController = new ProductController();
 
-    // 2 - Récupérer le produit
-    $db = new Database();
-
-    $stmt = $db->query(
-        "SELECT * FROM products WHERE productCode = ?",
-        [$_GET['productCode']]
-    );
-    $product = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if (empty($product)) {
-        echo '404 - no product found';
-        die;
-    }
-
-    // 3 - Afficher la page
-    include 'public/views/layout/header.view.php';
-    include 'public/views/product.view.php';
-    include 'public/views/layout/footer.view.php';
-
-} catch (Exception $e) {
-    print_r($e->getMessage());
-}
-
-
+$productController->show($_GET['productCode']);
